d80a9048a6dd97dd9bf62e590877ad1dfceb474d
JawalArcilla <>
2021-04-30 13:38:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let numebr = 0
+let sentence = 'this is the number 4'  
+
+document.getElementById('button').addEventListener('click', addition)
+
+function addition () {
+  number = document.getElementById('input').value
+
+  number = parseInt(number)
+
+  number = number * 3
+
+  alert(number)
+}
